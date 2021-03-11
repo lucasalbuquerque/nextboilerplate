d1b5fa32cc04218687476c1ecb7e66398bab25a9
Lucas Albuquerque <lucastableless@gmail.com>
2021-03-11 18:08:02 -0300

    setup new files

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,19 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>react avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS e StyledComponents
+    </S.Description>
+    <S.Illustration
+      src="img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+    />
+  </S.Wrapper>
 )
 export default Main

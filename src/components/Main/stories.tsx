e1b9a1fca43cad5f8f2a5c0ce832b406dcf0cfeb
Lucas Albuquerque <lucastableless@gmail.com>
2021-03-11 18:50:57 -0300

    adding storybook knobs configuration

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,9 +1,16 @@
+import { withKnobs, text } from '@storybook/addon-knobs'
 import { Meta } from '@storybook/react/types-6-0'
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main
+  component: Main,
+  decorators: [withKnobs]
 } as Meta
 
-export const Basic = () => <Main />
+export const Basic = () => (
+  <Main
+    title={text('Title', 'First title')}
+    description={text('Description', 'First Description')}
+  />
+)

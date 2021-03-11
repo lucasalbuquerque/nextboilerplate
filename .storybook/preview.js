e894f7d2cb0b6ded56609e9affe519fa8901d2dc
Lucas Albuquerque <lucastableless@gmail.com>
2021-03-11 18:25:52 -0300

    setup storybook globalstyles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,13 @@
+import GlobalStyles from '../src/styles/global';
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  )
+]
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
